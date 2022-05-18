9c181b4214aefa9619875d2825c76eb7c4f49141
pushpraj15295 <101568029+pushpraj15295@users.noreply.github.com>
2022-05-18 22:29:29 +0530

    add

---
+++ todo/src/components/TodoItem.jsx
@@ -0,0 +1,21 @@
+import React,{useState} from 'react'
+import styles from './todo.module.css';
+
+const TodoItem = ({todo , onDelete}) => {
+
+    const [isCompleted,setIsCompleted] = useState(todo.isCompleted);
+  return (
+    <div className={styles.todo} key = {todo.id}>
+              <input type="checkbox" 
+              checked={isCompleted}
+              onChange={(e) =>{
+                  setIsCompleted(e.target.checked);
+              }}/>
+            <div>{todo.value}</div>
+
+            <button onClick={()=> onDelete(todo.id)}>Delete</button>
+            </div>
+  )
+}
+
+export default TodoItem
--- todo/src/components/Todos.jsx
+++ todo/src/components/Todos.jsx
@@ -1,20 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import styles from './todo.module.css';
+import TodoItem from './TodoItem';
 
-const Todos = () => {
+const Todo = () => {
     let [value , setValue] = useState("")
     const [todos , setTodos] = useState([]);
+
+    const onDelete =()=>{
+        let newTodos = todos.filter((todo)=>todo.id !== id);
+
+        setTodos(newTodos);
+    };
+
   return (
     <div>Todo
 
-        <input value ={value} onChange={(e) => setValue(e.target.value)} />
-        <button onClick={() =>{setTodos([...todos,{value: value}]);setValue("")}}>
+        <input value ={value}
+         onChange={(e) => {
+             setValue(e.target.value)}} />
+
+        <button 
+        onClick={() =>{
+            setTodos([...todos,{value: value}]);
+            setValue("");
+            }}>
             Add
         </button>
-       
-       {todos.map((todo)=>
-         (<div>{todo.value}</div>))}
-    </div> 
+
+    <div className={styles.todoList}>
+        {todos.map((todo)=>(
+          <TodoItem key={todo.id} todo={todo} 
+          onDelete={onDelete}/>
+    ))}
+    </div>
+    </div>
+        
+        
   );
 };
 
-export default Todos
+export default Todo;
